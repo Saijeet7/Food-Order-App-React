b5dca88398e0577dffdef24df707e8abcb81e099
Saijeet Upadhyay <saijeet2019@gmail.com>
2023-10-31 18:25:21 +0545

    Complete mimor issues

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { React, Fragment, useState } from "react";
+import { React, useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <CartProvider>
-      {cartIsShown && <Cart onCloseCart={hideCartHandler} />}
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCard={showCardHandler} />
       <main>
         <Meals />
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,21 +1,26 @@
-import { React, useContext } from "react";
-import classes from "./Cart.module.css";
-import Modal from "../UI/Modal";
-import CartContext from "../../store/CartContext";
-import CartItem from "./CartItem";
+import { useContext } from 'react';
+
+import Modal from '../UI/Modal';
+import CartItem from './CartItem';
+import classes from './Cart.module.css';
+import CartContext from '../../store/CartContext';
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
-  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
 
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = (id) => {};
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
 
-  const cartItemAddHandler = (item) => {};
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({...item, amount: 1});
+  };
 
   const cartItems = (
-    <ul className={classes["cart-items"]}>
+    <ul className={classes['cart-items']}>
       {cartCtx.items.map((item) => (
         <CartItem
           key={item.id}
@@ -23,20 +28,21 @@ const Cart = (props) => {
           amount={item.amount}
           price={item.price}
           onRemove={cartItemRemoveHandler.bind(null, item.id)}
-          onAdd={cartItemAddHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
         />
       ))}
     </ul>
   );
+
   return (
-    <Modal onClose={props.onCloseCart}>
+    <Modal onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]} onClick={props.onCloseCart}>
+        <button className={classes['button--alt']} onClick={props.onClose}>
           Close
         </button>
         {hasItems && <button className={classes.button}>Order</button>}
